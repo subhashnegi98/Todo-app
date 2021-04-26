ef6390c30771718592d470bc325f1f3de94d2919
Subhash Negi <negisubhash25@gmail.com>
2021-04-26 08:46:15 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 ReactDOM.render(
-  <>
-  <React.StrictMode>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>
-  </>,
+  </Router>,
   document.getElementById('root')
 );
 reportWebVitals();
